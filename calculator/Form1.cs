d1751c2747f611bbe3f11cef36f041d068aaee02
가영 김 <kkw9082@naver.com>
2022-02-22 19:30:47 +0900

    ()*/ 추가하기

--- calculator/Form1.cs
+++ calculator/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace calculator
 {
@@ -20,18 +21,49 @@ namespace calculator
         private void button1_Click(object sender, EventArgs e)
         {
             string s = textBox1.Text;   //식을 문자열로 받을 변수
+            string str = RemoveNumber(s);   //숫자 없앰
+            string multi = "*";
+            string division = "/";
+            string plus = "+";
+            string minus = "-";
+            string[] index = {"+", "-", "*", "/","(",")"};
+            int indexMulti,indexDivision,indexPlus,indexMinus;
+
             int strIndex1 = s.LastIndexOf('+');
             int strIndex2 = s.LastIndexOf('-');
             string[] number;                  //+ -를 기준으로 나눠 저장
-            int count, result = 0;
+            int count;
+            double result=0;
+            //*,/ 만들기
+            //
+            if (s.Contains("(")){
+                int first = s.IndexOf('(');
+                int second = s.IndexOf(')');
+
+
+            }
+
+
+            if (s.Contains("*") && s.Contains("/")) {
+                indexMulti = s.IndexOf('*');
+                indexDivision = s.IndexOf('/');
+                if (indexMulti<indexDivision)
+                {
+                    count = s.Count(f => f == '*');
+                    number = s.Split('*');
+                    result =  Convert.ToDouble(number[0]);
+
+
+                }
+            }
             if (s.Contains("+"))
             {
                 count = s.Count(f => f == '+');
                 number = s.Split('+');
-                result = Convert.ToInt32(number[0]);
+                result = Convert.ToDouble(number[0]);
                 for (int i = 0; i < count + 1; i++)
                 {
-                    result += Convert.ToInt32(number[++i]);
+                    result += Convert.ToDouble(number[++i]);
                 }
                 textBox2.Text = result.ToString();
             }
@@ -39,10 +71,10 @@ namespace calculator
             {
                 count = s.Count(f => f == '-');
                 number = s.Split('-');
-                result = Convert.ToInt32(number[0]);
+                result = Convert.ToDouble(number[0]);
                 for (int i = 0; i < count + 1; i++)
                 {
-                    result -= Convert.ToInt32(number[++i]);
+                    result -= Convert.ToDouble(number[++i]);
                 }
                 textBox2.Text = result.ToString();
 
@@ -54,5 +86,16 @@ namespace calculator
         {
 
         }
+
+        public static string RemoveNumber(string str)
+        {
+            return Regex.Replace(str, @"\d", "");
+        }
+
+        public static double PlusMinus(double a,double b)
+        {
+            return 0;
+        }
+
     }
 }
